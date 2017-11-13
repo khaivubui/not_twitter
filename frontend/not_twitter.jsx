f92be9644b48d44735e65a8de70f29513d45fbbb
Khai Bui <khaivubui@gmail.com>
2017-11-13 13:39:21 -0800

    Set up redux store

--- frontend/not_twitter.jsx
+++ frontend/not_twitter.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const preloadedState = {};
+  const preloadedState = {
+    session: {
+      currentUser: null,
+    },
+    entities: {},
+    ui: {},
+    errors: []
+  };
 
-  // const store = configureStore(preloadedState);
-  // if (process.env.NODE_ENV !== 'production') {
-  //   window.store = store;
-  // }
+  const store = configureStore(preloadedState);
+  if (process.env.NODE_ENV !== 'production') {
+    window.store = store;
+  }
 
   ReactDOM.render(
     <Root/>,
