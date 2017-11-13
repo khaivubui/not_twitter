8170cb0ce0e320a895d5a7933bf75787d9909910
Khai Bui <khaivubui@gmail.com>
2017-11-13 14:13:58 -0800

    Add materializecss and set up basic routing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Home from './general/home';
+
 const App = () =>
 <Switch>
-  <Route exact path="/" component={() => <h1>HOME</h1>}/>
+  <Route exact path="/" component={Home}/>
   <Route path="/login" component={() => <h1>LOGIN</h1>}/>
   <Route path="/signup" component={() => <h1>SIGNUP</h1>}/>
 </Switch>;
