49ff018ac0a893bab821feaba3b89b242d2bddb1
Khai Bui <khaivubui@gmail.com>
2017-11-13 13:57:13 -0800

    Build up basic routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => <div>LOLOLOL</div>;
+const App = () =>
+<Switch>
+  <Route exact path="/" component={() => <h1>HOME</h1>}/>
+  <Route path="/login" component={() => <h1>LOGIN</h1>}/>
+  <Route path="/signup" component={() => <h1>SIGNUP</h1>}/>
+</Switch>;
 
 export default App;
