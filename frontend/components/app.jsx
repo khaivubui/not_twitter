54781cf6c8dbf7e60888b031ac4676432e15e92f
Khai Bui <khaivubui@gmail.com>
2017-11-13 14:38:24 -0800

    Build up login

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import Nav from './general/nav';
 
+import Login from './auth/login.jsx';
+
 const App = () =>
 <main>
   <Nav/>
   <Switch>
-    <Route path="/login" component={() => <h1>LOGIN</h1>}/>
+    <Route path="/login" component={Login}/>
     <Route path="/signup" component={() => <h1>SIGNUP</h1>}/>
   </Switch>
 </main>;
