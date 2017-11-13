2556b7905db250cb534f8e5b493b012045e24527
Khai Bui <khaivubui@gmail.com>
2017-11-13 11:02:33 -0800

    Add static folder and send root file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render("root"));
+app.use(express.static("./public"));
+
+app.get('/', (req, res) => res.sendfile("./public/root.html"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
