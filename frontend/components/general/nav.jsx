f114ca230a91091cb6b4575961e4e7eb6de069b1
Khai Bui <khaivubui@gmail.com>
2017-11-14 13:45:37 -0800

    Add logout functionality

--- frontend/components/general/nav.jsx
+++ frontend/components/general/nav.jsx
@@ -5,19 +5,32 @@ import { connect } from 'react-redux';
 import Cookies from 'js-cookie';
 
 export const GET_USER = 'GET_USER';
+export const LOGOUT = 'LOGOUT';
 
 class Nav extends React.Component {
   componentWillMount () {
     this.props.getUser();
   }
 
+  logOut () {
+    Cookies.set('Authorization', '');
+    this.props.logOut();
+  }
+
   render () {
+    const {currentUser} = this.props;
     return (
       <nav className="nav-wrapper">
         <Link to='/' className="left brand-logo">Not-Twitter</Link>
         <ul className="right">
-          <li><Link to='/login'>Login</Link></li>
-          <li><Link to='/register'>Register</Link></li>
+          <li hidden={currentUser ? false : true}>
+            Welcome back, {currentUser && currentUser.username}
+          </li>
+          <li hidden={currentUser ? false : true}>
+            <a onClick={_ => this.logOut()}>Log Out</a>
+          </li>
+          <li hidden={currentUser ? true : false}><Link to='/login'>Log In</Link></li>
+          <li hidden={currentUser ? true : false}><Link to='/register'>Register</Link></li>
         </ul>
       </nav>
     );
@@ -36,12 +49,15 @@ const getUserActionCreator = _ => {
   };
 };
 
+const logOutActionCreator = _ => ({type: LOGOUT});
+
 const mapStateToProps = state => ({
   currentUser: state.session.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-  getUser: _ => dispatch(getUserActionCreator())
+  getUser: _ => dispatch(getUserActionCreator()),
+  logOut: _ => dispatch(logOutActionCreator())
 });
 
 export default connect(
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,9 +6,11 @@ import Cookies from 'js-cookie';
 import RootReducer from '../reducers/root_reducer';
 
 const authMiddleWare = store => next => action => {
-  const data = action.payload.data;
-  if (data && data.user) {
-    Cookies.set('Authorization', `Bearer ${data.user.token}`);
+  if (action.payload) {
+    const data = action.payload.data;
+    if (data && data.user) {
+      Cookies.set('Authorization', `Bearer ${data.user.token}`);
+    }
   }
   next(action);
 };
