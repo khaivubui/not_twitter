74e1a9efd873a190867c1cdf754771b2be60f0dd
Khai Bui <khaivubui@gmail.com>
2017-11-14 13:18:19 -0800

    Change nav to a class

--- frontend/components/general/nav.jsx
+++ frontend/components/general/nav.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Nav = () =>
-<nav className="nav-wrapper">
-  <Link to='/' className="left brand-logo">Not-Twitter</Link>
-  <ul className="right">
-    <li><Link to='/login'>Login</Link></li>
-    <li><Link to='/register'>Register</Link></li>
-  </ul>
-</nav>;
+class Nav extends React.Component {
+  render () {
+    return (
+      <nav className="nav-wrapper">
+        <Link to='/' className="left brand-logo">Not-Twitter</Link>
+        <ul className="right">
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/register'>Register</Link></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
 
 export default Nav;
