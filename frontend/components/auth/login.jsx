73687cfd06d80dcb88112e3dbf063a9aab4ff9b6
Khai Bui <khaivubui@gmail.com>
2017-11-14 10:50:13 -0800

    Set up controlled form and fix styling bug

--- frontend/components/auth/login.jsx
+++ frontend/components/auth/login.jsx
@@ -1,26 +1,59 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      username: ''
+    };
+  }
+
+  update (e, fieldType) {
+    this.setState({
+      [fieldType]: e.target.value
+    });
+  }
+
   render () {
     return (
       <div className="row">
         <form className="col s12">
           <div className="row">
             <div className="input-field col s12">
-              <input id="email" type="email" className="validate"/>
-              <label htmlFor="email">Email</label>
+              <input id="email"
+                     type="email"
+                     className="validate"
+                     value={this.state.email}
+                     onChange={e => this.update(e, 'email')}/>
+              <label htmlFor="email" className="active">
+                Email
+              </label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
-              <input id="password" type="password" className="validate"/>
-              <label htmlFor="password">Password</label>
+              <input id="password"
+                     type="password"
+                     className="validate"
+                     value={this.state.password}
+                     onChange={e => this.update(e, 'password')}/>
+              <label htmlFor="password" className="active">
+                Password
+              </label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
-              <input id="username" type="text" className="validate"/>
-              <label htmlFor="username">Username</label>
+              <input id="username"
+                     type="text"
+                     className="validate"
+                     value={this.state.username}
+                     onChange={e => this.update(e, 'username')}/>
+              <label htmlFor="username" className="active">
+                Username
+              </label>
             </div>
           </div>
         </form>
