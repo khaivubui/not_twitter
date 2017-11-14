8dd08c4b152bc009039f8e26ddf5eda3ae9dede7
Khai Bui <khaivubui@gmail.com>
2017-11-14 10:55:23 -0800

    Add register form rendering

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,13 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import Nav from './general/nav';
 
 import Login from './auth/login.jsx';
+import Register from './auth/register.jsx';
 
 const App = () =>
 <main>
   <Nav/>
   <Switch>
     <Route path="/login" component={Login}/>
-    <Route path="/signup" component={() => <h1>SIGNUP</h1>}/>
+    <Route path="/register" component={Register}/>
   </Switch>
 </main>;
 
