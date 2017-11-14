f7dd6044848fe8069220057a03fde8452edaf9c4
Khai Bui <khaivubui@gmail.com>
2017-11-14 13:12:06 -0800

    Write boilerplate for authMiddleWare

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,11 +4,19 @@ import reduxPromise from 'redux-promise';
 
 import RootReducer from '../reducers/root_reducer';
 
+const authMiddleWare = store => next => action => {
+  const data = action.payload.data;
+  if (data && data.user) {
+    
+  }
+  next(action);
+};
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(reduxPromise, logger)
+    applyMiddleware(reduxPromise, authMiddleWare, logger)
   )
 );
 
