b161b05faf46b7b95c3ee1f7a8ac6c93f95073bc
Khai Bui <khaivubui@gmail.com>
2017-11-14 13:16:23 -0800

    Add jscookie functionality

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import reduxPromise from 'redux-promise';
-import Cookie from 'js-cookie';
+import Cookies from 'js-cookie';
 
 import RootReducer from '../reducers/root_reducer';
 
 const authMiddleWare = store => next => action => {
   const data = action.payload.data;
   if (data && data.user) {
-
+    Cookies.set('Authorization', `Bearer ${data.user.token}`);
   }
   next(action);
 };
