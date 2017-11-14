e66d864ad24bd1c47fc0ae7eaf4eb1da55af0628
Khai Bui <khaivubui@gmail.com>
2017-11-14 13:06:36 -0800

    Handle register

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,4 +1,5 @@
 import { LOGIN } from '../components/auth/login';
+import { REGISTER } from '../components/auth/register';
 
 const nullUser = {
   currentUser: null
@@ -8,6 +9,7 @@ const sessionReducer = (state = nullUser, action) => {
   Object.freeze(state);
   switch (action.type) {
     case LOGIN:
+    case REGISTER:
       const data = action.payload.data;
       if (data) {
         return {currentUser: data.user};
